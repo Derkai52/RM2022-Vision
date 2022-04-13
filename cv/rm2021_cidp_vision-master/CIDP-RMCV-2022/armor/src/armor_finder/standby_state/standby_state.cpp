2097fdd68234c6c993abeb49ad096a3e78e421f9
Derkai52 <2363104914@qq.com>
2022-04-14 00:30:40 +0800

    fixbug

---
+++ cv/rm2021_cidp_vision-master/CIDP-RMCV-2022/armor/src/armor_finder/standby_state/standby_state.cpp
@@ -0,0 +1,9 @@
+// 备用模式。待开发ing
+
+#include <armor_finder/armor_finder.h>
+
+bool ArmorFinder::stateStandBy() {
+    state = SEARCHING_STATE;
+    return true;
+}
+
---
+++ cv/rm2021_cidp_vision-master/CIDP-RMCV-2022/others/src/camera/video_wrapper.cpp
@@ -0,0 +1,17 @@
+#include "camera/video_wrapper.h"
+
+
+VideoWrapper::VideoWrapper(const std::string &filename) {
+    video.open(filename);
+}
+
+VideoWrapper::~VideoWrapper() = default;
+
+
+bool VideoWrapper::init() {
+    return video.isOpened();
+}
+
+bool VideoWrapper::read(cv::Mat &src) {
+    return video.read(src);
+}
